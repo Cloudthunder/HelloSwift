5e2624365c3a69784267e724cf8958ec7423bcd1
cloudthunder <Kchenpc@gmail.com>
2017-10-14 15:17:25 -0700

    Initial commit

---
+++ HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by UCode on 10/14/17.
+//  Copyright © 2017 Cloudthunder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
