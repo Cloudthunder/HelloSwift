1a89698bd850f92cfbaf40015a9dfe0c3fd74be6
cloudthunder <Kchenpc@gmail.com>
2017-10-14 15:21:22 -0700

    Add hello world code

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -1,12 +1,1 @@
-//
-//  main.swift
-//  HelloSwift
-//
-//  Created by UCode on 10/14/17.
-//  Copyright © 2017 Cloudthunder. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("Hello World!!!")
